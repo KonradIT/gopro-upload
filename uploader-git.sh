1094b75382ec51a8e9ae54254db0f2c87bab8939
Konrad Iturbe <github@chernowii.com>
2014-07-18 17:50:05 +0200

    Update uploader-git.sh

--- uploader-git.sh
+++ uploader-git.sh
@@ -1,2 +1,1 @@
-cd gopropictures
-wget -r -p -l inf -np http://10.5.5.9:8080/videos/DCIM/100GOPRO/ | git add . | git commit -am "`date`" | git push origin master | sleep X
+cd gopropictures | wget -r -p -l inf -np http://10.5.5.9:8080/videos/DCIM/100GOPRO/ | git add . | git commit -am "`date`" | git push origin master | sleep X
