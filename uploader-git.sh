fc8ef264c91cc511dd3fde45f9c5a4c6642e24b8
Konrad Iturbe <github@chernowii.com>
2014-07-19 19:22:51 +0200

    Update uploader-git.sh

--- uploader-git.sh
+++ uploader-git.sh
@@ -1,1 +1,10 @@
-iwconfig wlan0 essid GOPRO_NAME key GOPRO_PASSWORD | cd gopropictures | wget -r -p -l inf -np http://10.5.5.9:8080/videos/DCIM/100GOPRO/ | git add . | git commit -am "`date`" | git push origin master | sleep X
+   ####            ######                              #     #             ##                             #                   
+  #    #           #     #                             #     #              #                             #                   
+ #         #####   #     #   # ###    #####            #     #  ######      #      #####    ######   ######   #####    # ###  
+ #   ###  #     #  ######    ##      #     #           #     #  #     #     #     #     #  #     #  #     #  #     #   ##     
+ #     #  #     #  #         #       #     #           #     #  #     #     #     #     #  #     #  #     #  #######   #      
+  #    #  #     #  #         #       #     #           #     #  #     #     #     #     #  #    ##  #     #  #         #      
+   #####   #####   #         #        #####             #####   ######     ###     #####    #### #   ######   #####    #      
+                                                                #
+                                                                
+while :; iwlist wlan0 scan | nmcli dev wifi connect GoPro_SIDD password GoPro_PASSWORD | wget -r -p -l inf -np http://10.5.5.9:8080/videos/DCIM/100GOPRO/ | git add . | git commit -am "`date`" | git push origin master; done
