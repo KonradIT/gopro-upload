9eab34731843da7eca1cb8560dfd09f7aa77a8af
Konrad Iturbe <github@chernowii.com>
2014-07-19 19:20:24 +0200

    Update uploader-local.sh

--- uploader-local.sh
+++ uploader-local.sh
@@ -5,5 +5,6 @@
  #     #  #     #  #         #       #     #           #     #  #     #     #     #     #  #     #  #     #  #######   #      
   #    #  #     #  #         #       #     #           #     #  #     #     #     #     #  #    ##  #     #  #         #      
    #####   #####   #         #        #####             #####   ######     ###     #####    #### #   ######   #####    #      
-                                                               
+                                                                #
+
 while :; iwlist wlan0 scan | nmcli dev wifi connect GoPro_SIDD password GoPro_PASSWORD | wget -r -p -l inf -np -nc http://10.5.5.9:8080/videos/DCIM/100GOPRO/ || echo No Picture; done
