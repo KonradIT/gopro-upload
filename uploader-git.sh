26fa2bebbe90f30d2a739c6a1c43bebbceac9c75
Konrad Iturbe <github@chernowii.com>
2014-07-21 11:48:52 +0200

    Update uploader-git.sh

--- uploader-git.sh
+++ uploader-git.sh
@@ -7,4 +7,4 @@
    #####   #####   #         #        #####             #####   ######     ###     #####    #### #   ######   #####    #      
                                                                 #
                                                                 
-while true; do iwlist wlan0 scan; nmcli dev wifi connect GoPro_SSID password GoPro_PASSWORD; wget -l inf -np -r -nc http://10.5.5.9:8080/videos/DCIM/100GOPRO/; git add .; git commit -am "`date`"; git push origin master; done
+while true; do iwlist wlan0 scan; nmcli dev wifi connect GoPro_SSID password GoPro_PASSWORD; wget -l inf -np -nd -A JPG,jpg -r -N -nc http://10.5.5.9:8080/videos/DCIM/100GOPRO/; git add .; git commit -am "`date`"; git push origin master; done
