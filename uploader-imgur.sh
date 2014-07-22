676998220d201cd96ea73f535b6ce0da8e77a506
Konrad Iturbe <github@chernowii.com>
2014-07-22 19:34:00 +0200

    Update uploader-imgur.sh

--- uploader-imgur.sh
+++ uploader-imgur.sh
@@ -1,1 +1,10 @@
-imgur *.JPG > imgur-urls/"imgur-"`date +"%d-%m-%Y-_-%H-%M-%S"`.txt
+   ####            ######                              #     #             ##                             #                   
+  #    #           #     #                             #     #              #                             #                   
+ #         #####   #     #   # ###    #####            #     #  ######      #      #####    ######   ######   #####    # ###  
+ #   ###  #     #  ######    ##      #     #           #     #  #     #     #     #     #  #     #  #     #  #     #   ##     
+ #     #  #     #  #         #       #     #           #     #  #     #     #     #     #  #     #  #     #  #######   #      
+  #    #  #     #  #         #       #     #           #     #  #     #     #     #     #  #    ##  #     #  #         #      
+   #####   #####   #         #        #####             #####   ######     ###     #####    #### #   ######   #####    #      
+                                                                #
+                                                                
+while true; do iwlist wlan0 scan; nmcli dev wifi connect GoPro_SSID password GoPro_PASSWORD; wget -P gopro-pic -l inf -np -nd -A JPG,jpg -r -N -nc http://10.5.5.9:8080/videos/DCIM/100GOPRO/; imgur *.JPG > imgur-urls/"imgur-"`date +"%d-%m-%Y-_-%H-%M-%S"`.txt; rm *.JPG; done
